43abcfcd4772222e7d2af5d1158d44fd053a806e
lgm1007 <flxl654@gmail.com>
2024-03-17 20:09:50 +0900

    test: Calculator 클래스 나눗셈 기능 및 예외 테스트코드 작성

--- src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -1,10 +1,14 @@
 package com.group.libraryapp.calculator
 
+import java.lang.Exception
+
 fun main() {
     val calculatorTest = CalculatorTest()
     calculatorTest.addTest()
     calculatorTest.minusTest()
     calculatorTest.multiplyTest()
+    calculatorTest.divideTest()
+    calculatorTest.divideExceptionTest()
 }
 
 class CalculatorTest {
@@ -46,5 +50,35 @@ class CalculatorTest {
         if (calculator.number != 30) {
             throw IllegalArgumentException("기대한 테스트 결과와 일치하지 않습니다.")
         }
+    }
+
+    fun divideTest() {
+        // given
+        val calculator = Calculator(6)
+
+        // when
+        calculator.divide(2)
+
+        // then
+        if (calculator.number != 3) {
+            throw IllegalStateException("기대한 테스트 결과와 일치하지 않습니다.")
+        }
+    }
+
+    fun divideExceptionTest() {
+        // given
+        val calculator = Calculator(6)
+
+        // when
+        try {
+            calculator.divide(0)
+        } catch (e: IllegalArgumentException) {
+            // then 성공!
+            return
+        } catch (e: Exception) {
+            // then 실패
+            throw IllegalStateException("기대한 타입의 예외가 발생하지 않았습니다.")
+        }
+        throw IllegalStateException("기대한 예외가 발생하지 않았습니다.")
     }
 }
