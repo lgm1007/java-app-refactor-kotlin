741ab98105dd7f1cb4f17c7851669c53261055e6
lgm1007 <flxl654@gmail.com>
2024-04-10 16:04:50 +0900

    test: BookService 테스트코드 - loanBook 기능 테스트코드 작성

--- src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
@@ -1,6 +1,11 @@
 package com.group.libraryapp.service.book
 
+import com.group.libraryapp.domain.book.Book
 import com.group.libraryapp.domain.book.BookRepository
+import com.group.libraryapp.domain.user.User
+import com.group.libraryapp.domain.user.UserRepository
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistoryRepository
+import com.group.libraryapp.dto.book.request.BookLoanRequest
 import com.group.libraryapp.dto.book.request.BookRequest
 import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
 import org.junit.jupiter.api.AfterEach
@@ -13,6 +18,8 @@ import org.springframework.boot.test.context.SpringBootTest
 class BookServiceTest @Autowired constructor(
     private val bookService: BookService,
     private val bookRepository: BookRepository,
+    private val userRepository: UserRepository,
+    private val userLoanHistoryRepository: UserLoanHistoryRepository,
 ) {
 
     /**
@@ -21,6 +28,7 @@ class BookServiceTest @Autowired constructor(
     @AfterEach
     fun cleanDB() {
         bookRepository.deleteAll()
+        userRepository.deleteAll()
     }
 
     @Test
@@ -36,6 +44,25 @@ class BookServiceTest @Autowired constructor(
         val books = bookRepository.findAll()
         assertThat(books).hasSize(1)
         assertThat(books[0].name).isEqualTo("삼국지")
+    }
+
+    @Test
+    @DisplayName("책 대출 기능 테스트")
+    fun loadBookTest() {
+        // given
+        bookRepository.save(Book("삼국지"))
+        val saveUser = userRepository.save(User("lee", null))
+        val request = BookLoanRequest("lee", "삼국지")
+
+        // when
+        bookService.loanBook(request)
+
+        // then
+        val results = userLoanHistoryRepository.findAll()
+        assertThat(results).hasSize(1)
+        assertThat(results[0].bookName).isEqualTo("삼국지")
+        assertThat(results[0].user.id).isEqualTo(saveUser.id)
+        assertThat(results[0].isReturn).isFalse
     }
 
 }
